26b523813416d170eb43d47e60c93929707f2b38
wolfpat01 <haithem2001@gmail.com>
2020-07-29 00:48:07 +0100

    Updates

---
+++ precache-manifest.5b084af89454b516d1de7793777c3893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12e3b48bcc337d9dea5009eea004d42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d144a76d6efb03e5868a",
+    "url": "/static/css/main.36e23624.chunk.css"
+  },
+  {
+    "revision": "440a21fc3b7797a08599",
+    "url": "/static/js/2.1e93eaae.chunk.js"
+  },
+  {
+    "revision": "3a01609874c3b8a46250e8a81450568e",
+    "url": "/static/js/2.1e93eaae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d144a76d6efb03e5868a",
+    "url": "/static/js/main.e56880c2.chunk.js"
+  },
+  {
+    "revision": "21a0cf4c4edef7d85275",
+    "url": "/static/js/runtime-main.23b727f6.js"
+  }
+]);
